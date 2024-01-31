5651f8357b8ba2e119a2f4461f58e1e58542a4a3
lasyaMundrathi <98383338+lasyaMundrathi@users.noreply.github.com>
2024-01-31 10:08:10 +0000

    Time: 2143 ms (74.74%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -1,19 +1,16 @@
-select name AS results  from (
-    select name,u1.user_id,count(rating) as greatestmovies from MovieRating l1
-join Users u1 on l1.user_id=u1.user_id
-group by user_id      
-order by greatestmovies desc, name ASC limit 1
-) t1
+(SELECT u1.name AS results
+FROM Users u1
+JOIN MovieRating l1 ON u1.user_id = l1.user_id
+GROUP BY u1.user_id, u1.name
+ORDER BY COUNT(l1.rating) DESC, u1.name ASC 
+LIMIT 1)
 
-union ALL
-select tmp.title AS results  from
-(
-    SELECT m1.movie_id, m2.title, AVG(m1.rating) AS avg_rating 
-FROM MovieRating m1
-JOIN Movies m2 ON m1.movie_id = m2.movie_id 
-where DATE_FORMAT(created_at, '%Y%m') = '202002'
-GROUP BY m1.movie_id, m2.title
-ORDER BY avg_rating DESC,M2.title ASC
-LIMIT 1
-)tmp 
+UNION ALL
 
+(SELECT m2.title AS results
+FROM Movies m2
+JOIN MovieRating m1 ON m2.movie_id = m1.movie_id
+WHERE m1.created_at BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY m2.movie_id, m2.title
+ORDER BY AVG(m1.rating) DESC, m2.title ASC
+LIMIT 1);
